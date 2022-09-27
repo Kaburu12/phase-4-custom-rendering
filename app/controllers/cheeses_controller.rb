3a37d549015d863e5f0b6a0022a4f610af241e24
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-09-27 06:09:47 -0400

    commit tree c23bbaaf44f4ac0503952de75c49a601f8b6f544

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,13 +3,21 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    # render json: cheeses
+    # render json: cheeses , only:[:id, :name, :price]
+    # render json: cheeses , except:[:created_at, :updated_at]
+    render json: cheeses , except:[:created_at, :updated_at], methods:[:summary]
   end
 
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    #custome render message
+    if cheese
+    render json: cheese,except:[:created_at, :updated_at]
+    else
+      render json: {error: "Cheese not found"}
+    end
   end
 
 end
